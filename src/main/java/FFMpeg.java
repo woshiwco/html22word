3c874806ad2c7c0afd7df36b06127c24719bec1c
joey <joey.wang@parrowtech.com>
2022-11-02 11:48:48 +0800

    添加注释112345

--- src/main/java/FFMpeg.java
+++ src/main/java/FFMpeg.java
@@ -19,7 +19,7 @@ public class FFMpeg {
             e.printStackTrace();
         }
     }
-    // 截取图片，12224
+    // 截取图片，122245
     public static void fetchPic(File originVideo, String targetPath, Integer ignoreFrame, Integer width) throws Exception {
         ignoreFrame = ignoreFrame == null ? 5 : ignoreFrame;
         width = width == null ? 640 : width;
